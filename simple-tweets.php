bd1dc2ff8550530874c58c9fbb1a276b2a340e5c
Constantine Kiriaze <ckiriaze@gmail.com>
2013-11-27 15:15:11 -0800

    init

---
+++ simple-tweets.php
@@ -0,0 +1,73 @@
+<?php
+/**
+    *
+    * @package   Simple Tweets
+    * @author    Constantine Kiriaze, hello@kiriaze.com
+    * @license   GPL-2.0+
+    * @link      http://getsimple.io
+    * @copyright 2013 Constantine Kiriaze
+    *
+	* Plugin Name:     Simple Tweets
+	* Plugin URI:      http://getsimple.io
+	* Description:     Simple Tweets
+	* Version:         1.0
+	* Author:          Constantine Kiriaze (@kiriaze)
+	* Author URI:      http://getsimple.io/about
+    * Text Domain:     'simple' 
+    * Copyright:       (c) 2013, Constantine Kiriaze
+    * License:         GNU General Public License v2 or later
+    * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/*
+    Copyright (C) 2013  Constantine Kiriaze (hello@kiriaze.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'SIMPLE_TWEETS_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define( 'SIMPLE_TWEETS_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+define( 'SIMPLE_TWEETS_PLUGIN_BASENAME', plugin_basename(__FILE__));
+
+//  Wrapped in after_setup_theme to utilize options
+add_action('after_setup_theme', 'simple_tweets_plugin_init');
+function simple_tweets_plugin_init(){
+
+    // Check theme options for values below
+
+    // Setting our Authentication Variables that we got after creating an application
+    $oauth_access_token = "16978818-X6wjOuO6WdsOtd4NPkuiG0AXtJ0FC8AGBBSOPB70a";
+    $oauth_access_token_secret = "9s86WB7CzbQ7I4wGgnVFI8luzVe6l9UbOb0q8J4d4";
+    $consumer_key = "V4izD5vfx51znhPXaDGxdQ";
+    $consumer_secret = "i9ACofSBMGBGCGzen81mQ6Evr7z5I0otFZKiiavQiOY";
+    $username = "kiriaze";
+    $count = 3;
+    $cache = 15; // number of minutes, defaults to 15min
+
+    define( 'oauth_access_token', $oauth_access_token);
+    define( 'oauth_access_token_secret', $oauth_access_token_secret);
+    define( 'consumer_key', $consumer_key);
+    define( 'consumer_secret', $consumer_secret);
+    define( 'username', $username);
+    define( 'count', $count);
+    define( 'cache', $cache * MINUTE_IN_SECONDS );
+
+    //  Load class
+    require_once( SIMPLE_TWEETS_PLUGIN_PATH . 'class-simple-tweets.php' );
+}
